5b76ae18dd9a2026bbb1d0136bff280802e86a1b
Frari <francescoriccardi86@gmail.com>
2019-06-14 15:24:24 +0200

    fine

--- index.php
+++ index.php
@@ -33,7 +33,24 @@
         'domanda' => "Quando faccio clic sui risultati della Ricerca Google, le mie chiavi di ricerca vengono inviate ai siti web?",
 
         'risposta' => "<p>In alcuni casi sì. Quando fai clic su un risultato della Ricerca Google, il tuo browser web potrebbe reindirizzare alla pagina web di destinazione anche l'indirizzo Internet, o URL, della pagina dei risultati di ricerca sotto forma di URL referrer. Talvolta, l'URL della pagina dei risultati di ricerca potrebbe contenere la query di ricerca che hai inserito. Se utilizzi la ricerca SSL (la funzione di ricerca criptata di Google), nella maggior parte dei casi i termini di ricerca non vengono inviati come parte dell'URL negli URL referrer. Questo comportamento può fare eccezione, ad esempio se utilizzi alcuni browser meno diffusi. Ulteriori informazioni sulla ricerca SSL sono disponibili qui. Le query di ricerca o le informazioni contenute nell'URL referrer potrebbero essere disponibili mediante Google Analytics o un'API (Application Programming Interface). Inoltre, gli inserzionisti potrebbero ricevere informazioni relative all' esatte parole chiave che hanno determinato il clic su un annuncio.</p>",
-    ],
+    ]
 
-  ]
+  ];
  ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Faq Google</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <?php
+      foreach ($faq as $key => $value) {?>
+        <div class="container"><h2><?php echo $value['domanda']; ?></h2></div>
+        <div class="container"><?php echo $value['risposta']; ?></p></div>
+
+    <?php }
+     ?>
+  </body>
+</html>
